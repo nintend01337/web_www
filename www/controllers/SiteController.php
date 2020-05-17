5a52f9304e3eae59d21f9c415c5eb5cc8e223761
nintend01337 <horunzhiy11@yandex.ru>
2020-05-17 19:19:18 +0300

    gg

--- www/controllers/SiteController.php
+++ www/controllers/SiteController.php
@@ -71,6 +71,7 @@ class SiteController extends Controller
      */
     public function actionLogin()
     {
+        $model = new User();   // ???
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
